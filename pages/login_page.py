d5a0c0149a5faca6a49d1c3b9a7989d0d8fde60b
capitansmolet <86357990+capitansmolet@users.noreply.github.com>
2021-06-25 15:18:26 +0300

    for review module 4

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,18 +1,32 @@
-from pages.locators import *
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
 
 class LoginPage(BasePage):
-    def should_be_login_page(self):
+    def register_new_user(self, email: str, password: str) -> None:
+        self.browser.find_element(*LoginPageLocators.EMAIL_INPUT).send_keys(
+            email)
+        self.browser.find_element(*LoginPageLocators.PASSWORD_INPUT).send_keys(
+            password)
+        self.browser.find_element(
+            *LoginPageLocators.REPEAT_PASSWORD_INPUT).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON).click()
+
+    def should_be_login_page(self) -> None:
         self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
 
-    def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert "/login/" in self.browser.current_url, "word \"login\" not in url"
+    def should_be_login_url(self) -> None:
+        current_url = self.browser.current_url
+        assert "login" in current_url, f"No login substring in the {current_url}"
 
-    def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+    def should_be_login_form(self) -> None:
+        assert self.is_element_present(
+            *LoginPageLocators.LOGIN_FORM
+        ), f"Login form is not present on the {self.browser.current_url}"
 
-    def should_be_register_form(self):
-        assert self.is_element_present(*LoginPageLocators), "Login link is not presented1"
+    def should_be_register_form(self) -> None:
+        assert self.is_element_present(
+            *LoginPageLocators.REGISTER_FORM
+        ), f"Register form is not present on the {self.browser.current_url} page"
