8e3f91d5d7e99bb8903dbd7d2e2d46700cf3578f
capitansmolet <86357990+capitansmolet@users.noreply.github.com>
2021-06-25 15:17:22 +0300

    for review module 4

--- pages/base_page.py
+++ pages/base_page.py
@@ -12,27 +12,14 @@ from .locators import BasePageLocators
 
 class BasePage:
     def __init__(self, browser: Remote, url: str) -> None:
-        """Base class for the web page
-        :param selenium.webdriver.Remote browser: Selenium WebDriver instance
-        :param str url: Link to the page
-        """
         super().__init__()
         self.browser = browser
         self.url = url
 
     def open(self) -> None:
-        """Open the given web page in the browser
-        :return: None
-        """
         self.browser.get(self.url)
 
     def is_element_present(self, how: str, what: str, timeout=4) -> bool:
-        """
-        :param how:
-        :param what:
-        :param timeout:
-        :return:
-        """
         try:
             WebDriverWait(self.browser, timeout).until(
                 ExpCond.presence_of_element_located((how, what)))
