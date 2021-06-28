14a0f2421817ef2ef22974062c866f8ca9b78a6a
capitansmolet <86357990+capitansmolet@users.noreply.github.com>
2021-06-28 15:33:28 +0300

    for module4

--- test_main_page.py
+++ test_main_page.py
@@ -1,26 +1,29 @@
 import pytest
 
-from pages.cart_page import CartPage
-from pages.login_page import LoginPage
-from pages.main_page import MainPage
-
-main_page_link = "http://selenium1py.pythonanywhere.com"
+from .pages.basket_page import BasketPage
+from .pages.locators import LinksLocators
+from .pages.login_page import LoginPage
+from .pages.main_page import MainPage
 
 
 @pytest.mark.login_guest
 class TestLoginFromMainPage:
-    def test_guest_can_go_to_login_page(self, browser) -> None:
-        main_page = MainPage(browser, main_page_link)
-        main_page.open()
-        main_page.go_to_login_page()
-        main_page.should_be_login_link()
+    def test_guest_can_go_to_login_page(self, browser):
+        page = MainPage(browser, LinksLocators.MAIN_LINK)
+        page.open()
+        page.go_to_login_page()
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
 
-    def test_guest_should_see_login_link(self, browser) -> None:
-        main_page = MainPage(browser, main_page_link)
-        main_page.open()
-        main_page.go_to_cart_page()
-        cart_page = CartPage(browser, browser.current_url)
-        cart_page.should_be_empty()
-        cart_page.should_contain_empty_text()
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, LinksLocators.MAIN_LINK)
+        page.open()
+        page.should_be_login_link()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    page = MainPage(browser, LinksLocators.MAIN_LINK)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, LinksLocators.BASKET_LINK)
+    basket_page.should_be_basket_empty()
