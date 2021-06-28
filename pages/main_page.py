d0b3bd78ae4ccfb5ec17aa55da9d9dd263b01c29
capitansmolet <86357990+capitansmolet@users.noreply.github.com>
2021-06-28 15:36:55 +0300

    for module4

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,9 +1,5 @@
-from selenium.webdriver import Remote
-
 from .base_page import BasePage
 
 
 class MainPage(BasePage):
-
-    def __init__(self, browser: Remote, url: str) -> None:
-        super().__init__(browser, url)
+    pass
